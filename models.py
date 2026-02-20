31d73577265f58136b3efc61cbb4bc97b68a6b79
AceGotik <ilyakashavy@gmail.com>
2026-02-20 20:02:14 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,13 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Place(Base):
-    __tablename__ = "places_new"
+    __tablename__ = "places"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    category = Column(String)
-    description = Column(Text)
-    rating = Column(Integer, default=0)
     average_price = Column(Integer)
     street = Column(String)
     type = Column(String)
     work_time = Column(String)
+    rating = Column(Integer, default=0)
