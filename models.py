83cba267de8068d51a00b054741f963ef70404a4
AceGotik <ilyakashavy@gmail.com>
2026-02-20 21:14:09 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, index=True)
+
 
 class Place(Base):
     __tablename__ = "places"
@@ -12,3 +21,20 @@ class Place(Base):
     work_time = Column(String)
     rating = Column(Integer, default=0)
     image = Column(String)
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    place_id = Column(Integer, ForeignKey("places.id"))
+    value = Column(Integer)
+
+
+class Favorite(Base):
+    __tablename__ = "favorites"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    place_id = Column(Integer, ForeignKey("places.id"))
