179d3e95cd3361492ef6210b20cbbc498caa8468
AceGotik <ilyakashavy@gmail.com>
2026-02-20 20:37:50 +0300

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class Place(Base):
-    __tablename__ = "places_full"
+    __tablename__ = "places"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
