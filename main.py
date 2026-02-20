38167b812df0934ef557ce001d11d73b9eb3f4d7
AceGotik <ilyakashavy@gmail.com>
2026-02-20 16:42:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from database import engine, Base
+from models import Place
+from sqlalchemy.orm import Session
+from database import SessionLocal
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"message": "City Places Backend Running"}
+
+@app.get("/places")
+def get_places():
+    db: Session = SessionLocal()
+    places = db.query(Place).all()
+    return places
