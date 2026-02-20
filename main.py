2c89fbbbe78ac7e5737b53f73b98213875a0dd4e
AceGotik <ilyakashavy@gmail.com>
2026-02-20 16:50:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,24 @@ def get_places():
     db: Session = SessionLocal()
     places = db.query(Place).all()
     return places
+
+from pydantic import BaseModel
+
+class PlaceCreate(BaseModel):
+    name: str
+    category: str | None = None
+    description: str | None = None
+
+
+@app.post("/places")
+def create_place(place: PlaceCreate):
+    db = SessionLocal()
+    new_place = Place(
+        name=place.name,
+        category=place.category,
+        description=place.description
+    )
+    db.add(new_place)
+    db.commit()
+    db.refresh(new_place)
+    return new_place
